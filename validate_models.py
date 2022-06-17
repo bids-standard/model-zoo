8990ae63db51d964b00e32d611e90ebec0bc4326
Alejandro de la Vega <aleph4@gmail.com>
2022-06-17 15:51:36 +0100

    Update requirements

--- validate_models.py
+++ validate_models.py
@@ -6,7 +6,6 @@ import sys
 all_models = Path('.').glob('*/*smdl.json')
 status = 0
 for i in all_models:
-    status = '✓'
     try:
         BIDSStatsModel.parse_file(str(i))
     except ValidationError as e:
@@ -15,7 +14,6 @@ for i in all_models:
        status = 1
     else:
         print(f"Model: {i.absolute()}, passed ✓")
-
 
 sys.exit(status)
 
