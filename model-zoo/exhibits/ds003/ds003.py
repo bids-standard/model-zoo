6323cf09113b31c31a21f1ea03430e3a36db4e0b
Alejandro de la Vega <aleph4@gmail.com>
2022-09-23 12:42:12 -0500

    Update

--- model-zoo/exhibits/ds003/ds003.py
+++ model-zoo/exhibits/ds003/ds003.py
@@ -24,8 +24,6 @@ from itertools import chain
 import numpy as np
 import pandas as pd
 
-
-
 # %% [markdown]
 # # Preamble
 # We will use Datalad to manage our data, which in turn depends on git-annex. To install git-annex, uncomment and run one of the following:
@@ -50,25 +48,25 @@ import pandas as pd
 # We will also install unreleased versions of Datalad, Pybids, and [Nilearn](https://nilearn.github.io/) to make things run a little more smoothly.
 
 # %%
-# ! pip install -q --upgrade pip setuptools
-# ! pip install -q --upgrade datalad nistats pybids graphviz
+# # ! pip install -q --upgrade pip setuptools
+# # ! pip install -q --upgrade datalad nistats pybids graphviz
 
 
 # %%
-# get the dataset
-# !export DATALAD_UI_PROGRESSBAR=log
-# !export NO_COLOR="1"
+# # get the dataset
+# # !export DATALAD_UI_PROGRESSBAR=log
+# # !export NO_COLOR="1"
 
-# !datalad install -r ds000003-fmriprep/
-# !datalad update ds000003_fmriprep
+# # !datalad install -r ds000003-fmriprep/
+# # !datalad update ds000003_fmriprep
 
-# !datalad install -r ds000003/
-# !datalad update ds000003
+# # !datalad install -r ds000003/
+# # !datalad update ds000003
 
 # %%
-# ! datalad get ds000003-fmriprep/sub-*/func/*_desc-confounds_*.tsv \
-#               ds000003-fmriprep/sub-*/func/*_desc-confounds_*.json \
-#               ds000003-fmriprep/dataset_description.json 
+# # ! datalad get ds000003-fmriprep/sub-*/func/*_desc-confounds_*.tsv \
+# #               ds000003-fmriprep/sub-*/func/*_desc-confounds_*.json \
+# #               ds000003-fmriprep/dataset_description.json 
 
 # %%
 from nilearn.plotting import plot_design_matrix
@@ -97,9 +95,6 @@ layout
 graph = BIDSStatsModelsGraph(layout, spec)
 
 # %%
-graph.write_graph(format='svg')
-
-# %%
 root_node = graph.root_node
 
 # %%
@@ -109,148 +104,33 @@ except ValueError:
     graph.load_collections(scan_length=320)
 
 # %%
-graph.load_collections(scan_length=320)
+collections = layout.get_collections('run', task='rhymejudgment', scan_length=320)
 
 # %%
-foo = graph.root_node.get_collections()[0]
+root_node.get_collections()[-1].entities
 
 # %%
-[ff.run_info[0][0]['RepetitionTime'] for ff in list(foo.variables.values())]
+# graph.load_collections(scan_length=320)
 
 # %%
-for ff in list(foo.variables.values()):
-    break
+# foo = graph.root_node.get_collections()[0]
 
 # %%
-ff.to_df()
+# [ff.run_info[0][0]['RepetitionTime'] for ff in list(foo.variables.values())]
+
+# %%
+# for ff in list(foo.variables.values()):
+#     break
+
+# %%
+# ff.to_df()
 
 # %%
 specs = root_node.run(group_by=root_node.group_by)
 len(specs)
 
 # %%
-api(specs[0])
-
-# %%
 specs[0]
 
 # %%
-specs[0].X
-
-# %%
 plot_design_matrix(specs[0].X, rescale=False)
-
-# %%
-specs[0].entities
-
-# %%
-specs[0].metadata
-
-# %%
-bold = layout.get(**specs[0].entities, suffix='bold', extension='.nii.gz')[0]
-
-# %%
-# import nilearn.glm
-# l1m = nilearn.glm.first_level.FirstLevelModel()
-# l1m.fit(bold.get_image(), design_matrices=specs[0].X)
-
-# %%
-next_node = root_node.children[0].destination
-
-# %%
-next_node.group_by
-
-# %%
-root_node.children[0].filter
-
-# %%
-contrasts = list(chain(*[s.contrasts for s in specs]))
-sub_specs = next_node.run(contrasts, group_by=next_node.group_by)
-
-# %%
-api(sub_specs[3])
-
-# %%
-# %pdb
-
-# %%
-ds1_node = next_node.children[1].destination
-api(ds1_node)
-
-# %%
-next_node.children[1].filter
-
-# %%
-sub_contrasts = list(chain(*[s.contrasts for s in sub_specs]))
-ds1_specs = ds1_node.run(sub_contrasts, group_by=ds1_node.group_by, **next_node.children[1].filter)
-
-# %%
-ds1_specs[0].X
-
-# %%
-pd.concat((ds1_specs[0].data, ds1_specs[0].metadata), axis=1)
-
-# %%
-ds1_specs[0].contrasts
-
-# %%
-ds0_node = next_node.children[0].destination
-ds0_specs = ds0_node.run(sub_contrasts, group_by=ds0_node.group_by)
-
-# %%
-ds0_specs
-
-# %%
-ds0_specs[1].X
-
-# %%
-pd.concat((ds0_specs[0].data, ds0_specs[0].metadata), axis=1)
-
-# %%
-ds2_node = next_node.children[2].destination
-filters = next_node.children[2].filter or {}
-print(filters)
-ds2_specs = ds2_node.run(sub_contrasts, group_by=ds2_node.group_by, **filters)
-print(ds2_specs)
-
-# %%
-api(ds2_specs[0])
-
-# %%
-ds2_specs[0].X
-
-# %%
-pd.concat((ds2_specs[0].data, ds2_specs[0].metadata), axis=1)
-
-# %%
-api(ds1_node)
-
-# %%
-graph.nodes
-
-# %%
-graph.root_node.children
-
-# %%
-graph.root_node.children[0].destination.children
-
-# %%
-graph.root_node.children[0].destination.children[2].destination.name
-
-# %%
-© 2022
-GitHub, Inc.
-Terms
-Privacy
-Security
-Status
-Docs
-Contact
-GitHub
-Pricing
-API
-Training
-Blog
-About
-Loading
-complete
